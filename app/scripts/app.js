8bf20ffb8d9c1310a54a417afd080efe5d07744a
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-05-19 14:45:51 +0900

    削除機能を追加

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -97,7 +97,14 @@ var Marionette = require('backbone.marionette');
 
 module.exports = Marionette.ItemView.extend({
     tagName: 'ul',
-    template: '#comment_view'
+    template: '#comment_view',
+    events: {
+        'click .delete': 'onClickDelete'
+    },
+    onClickDelete: function(e) {
+        e.preventDefault();
+        this.model.destroy();
+    }
 });
 
 
--- app/scripts/views/CommentView.js
+++ app/scripts/views/CommentView.js
@@ -2,6 +2,13 @@ var Marionette = require('backbone.marionette');
 
 module.exports = Marionette.ItemView.extend({
     tagName: 'ul',
-    template: '#comment_view'
+    template: '#comment_view',
+    events: {
+        'click .delete': 'onClickDelete'
+    },
+    onClickDelete: function(e) {
+        e.preventDefault();
+        this.model.destroy();
+    }
 });
 
